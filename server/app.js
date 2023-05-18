bd8390b9ba290c307297ba82e3841d62b4b706de
Keerty Verma <keertyverma1303@gmail.com>
2023-05-18 17:04:38 +0530

    Add configuration file

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 const express = require("express");
+const config = require("config");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => {
+app.get(`/${config.get("server.app_name")}/`, (req, res) => {
   res.send("<h1>Welcome to MindMemo app</h1>");
 });
 
