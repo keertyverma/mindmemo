dcdef4a4c858f4d3e0ba9cd80775d053a893e9a0
Keerty Verma <keertyverma1303@gmail.com>
2023-05-18 16:47:05 +0530

    Pick PORT and NODE_ENV from .env file

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
+require("dotenv").config({ path: __dirname + "/.env" });
 const express = require("express");
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to MindMemo app</h1>");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on PORT:3000");
+app.listen(PORT, () => {
+  console.log(`App enviornment = ${process.env.NODE_ENV}`);
+  console.log(`Server is running on PORT:${PORT}`);
 });
