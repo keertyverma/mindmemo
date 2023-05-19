ecddbd2cbe565cd43de76089ad4aa05a44d8bf00
Keerty Verma <keertyverma1303@gmail.com>
2023-05-19 11:54:50 +0530

    Create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
+import "./App.css";
+import Header from "./components/Header";
+
 function App() {
-  return "MindMemo app";
+  return <Header />;
 }
 
 export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { MdOutlineHighlight } from "react-icons/md";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <MdOutlineHighlight />
+        MindMemo
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
