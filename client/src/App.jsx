b5502669e602c143845e3b6547b04a9a2500eefe
Keerty Verma <keertyverma1303@gmail.com>
2023-05-19 14:22:04 +0530

    Create Note component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NoteForm from "./components/NoteForm/NoteForm";
+import Note from "./components/Note/Note";
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -13,6 +14,22 @@ function App() {
       <NoteForm
         onAdd={(newNote) => setNotes((prevNotes) => [newNote, ...prevNotes])}
       />
+      <div className="notes">
+        {notes.map((note, index) => (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={(id) =>
+              setNotes((prevNotes) =>
+                prevNotes.filter((note, index) => index !== id)
+              )
+            }
+          />
+        ))}
+      </div>
+
       <Footer />
     </>
   );
---
+++ client/src/components/Note/Note.jsx
@@ -0,0 +1,20 @@
+import { MdDelete } from "react-icons/md";
+import "./Note.css";
+
+function Note({ id, title, content, onDelete }) {
+  return (
+    <div className="note">
+      <h2 className="title">{title}</h2>
+      <p className="content">{content}</p>
+      <button
+        onClick={() => {
+          onDelete(id);
+        }}
+      >
+        <MdDelete />
+      </button>
+    </div>
+  );
+}
+
+export default Note;
