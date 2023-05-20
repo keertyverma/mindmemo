46dc1c7b3487a76bf30277c31df31464ab71d4a9
Keerty Verma <keertyverma1303@gmail.com>
2023-05-20 15:03:43 +0530

    Handle note content overflow

--- client/src/components/Note/Note.jsx
+++ client/src/components/Note/Note.jsx
@@ -8,17 +8,24 @@ function Note({ id, title, content }) {
   return (
     <>
       <div className="note">
-        <h2 className="title">{title}</h2>
-        <p className="content">{content}</p>
-        {deleteNote.error && (
-          <p className="deletenote-error">Failed to delete note!</p>
+        {deleteNote.isLoading ? (
+          <p>Deleting...</p>
+        ) : (
+          deleteNote.error && (
+            <p className="deletenote-error">Failed to delete note!</p>
+          )
         )}
-        <button
-          onClick={() => deleteNote.mutate(id)}
-          disabled={deleteNote.isLoading}
-        >
-          <MdDelete />
-        </button>
+
+        <div className="note-header">
+          <h2 className="title">{title}</h2>
+          <button
+            onClick={() => deleteNote.mutate(id)}
+            disabled={deleteNote.isLoading}
+          >
+            <MdDelete />
+          </button>
+        </div>
+        <div className="note-content"> {content}</div>
       </div>
     </>
   );
