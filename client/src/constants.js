da7c85ae74672435bf183f5b449b390720ae51a9
Keerty Verma <keertyverma1303@gmail.com>
2023-05-20 18:17:27 +0530

    Read appwrite details from .env file

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,11 +1,9 @@
-//TODO: move config details to .env file
-
 const constant = {
   appwrite: {
     endpoint: "https://cloud.appwrite.io/v1",
-    projectID: "6468736bb211a95fd5f1",
-    databaseID: "64689bca7681d4ae696b",
-    collectionID: "64689bedeec485348264",
+    projectID: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    databaseID: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    collectionID: import.meta.env.VITE_APPWRITE_COLLECTION_ID,
   },
 };
 
