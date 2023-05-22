ba196c29cf48f58ed19abb78b790eee73e5fbf0b
Keerty Verma <keertyverma1303@gmail.com>
2023-05-22 14:37:45 +0530

    Setup react router for navigation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
 
 import "./index.css";
-import App from "./App.jsx";
+import router from "./routes";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
